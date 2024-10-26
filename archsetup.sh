8c1377728d2b316912662943811207add5fc9c3a
Harshal Sawant <harshalsawant2004h@gmail.com>
2024-10-26 15:03:12 +0530

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,47 +1,58 @@
 #!/bin/bash
-sudo pacman -Syu --noconfirm #update
-sudo pacman -S --noconfirm base-devel git wget curl
-#
+set -e
+set -o pipefail
+
+# System update and base packages
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel git
+
+# Install yay
 if ! command -v yay &>/dev/null; then
-  git clone https://aur.archlinux.org/yay.git
-  cd yay 
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay
-fi;
-#
-yay # Update
-#
-#
-# /etc/pacman.conf ,parrelel downloading, colour enable.
-#
+    git clone https://aur.archlinux.org/yay.git
+    cd yay 
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay
+fi
 
-yay -S --noconfirm \
-  brave-bin \
-  telegram-desktop \
-  github-desktop \
-  visual-studio-code-bin \
-  ferdium-bin \
-  zoom \
-  cmake \
-  android-ndk \
-  openjdk-src
-#
+# Update AUR
+yay -Syu --noconfirm
 
-sudo pacman -S --noconfirm \
-  neovim \
-  neofetch \
-  htop \
-  fastfetch \
-  docker \
-  nodejs \
-  npm \
-  discord \
-  libreoffice
+# Configure pacman
+sudo sed -i '/\[options\]/a ParallelDownloads = 5' /etc/pacman.conf
+sudo sed -i '/\[options\]/a Color' /etc/pacman.conf
 
+# Remove conflicts and install AUR packages
+yay -S --needed --noconfirm \
+    brave-bin \
+    telegram-desktop \
+    github-desktop \
+    visual-studio-code-bin \
+    ferdium-bin \
+    zoom \
+    cmake \
+    android-ndk \
+    openjdk-src
+
+# Install official packages
+sudo pacman -S --needed --noconfirm \
+    neovim \
+    neofetch \
+    htop \
+    fastfetch \
+    docker \
+    nodejs \
+    npm \
+    discord \
+    libreoffice
+
+# Enable services
 sudo systemctl enable fstrim.timer
+sudo systemctl enable docker.service
 
+# Git configuration
 git config --global user.name "c0d3h01"
 git config --global user.email "harshalsawant2004h@gmail.com"
 
-#######################
+# Clean package cache
+yay -Sc --noconfirm
