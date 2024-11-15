d5b1237c1ac2d6c9032e7cf46a44cbedc6e472fb
Harshal Sawant <harshalsawant2004h@gmail.com>
2024-11-15 17:29:27 +0530

    New partitioning method

--- archinstall.sh
+++ archinstall.sh
@@ -1,55 +1,72 @@
 #!/bin/bash
 
-set -e
-
+# Define drive and mount points
 DRIVE="/dev/nvme0n1"
+EFI_PART="${DRIVE}p1"
+ROOT_PART="${DRIVE}p2"
 
-# Clear and create partitions
-sgdisk -Z ${DRIVE}
-sgdisk -o ${DRIVE}
-sgdisk -n 1:0:+1GB -t 1:ef00 -c 1:"EFI" \
-       -n 2:0:0 -t 2:8300 -c 2:"ROOT" ${DRIVE}
+# Clear all partition data and GPT/MBR structures
+sgdisk --zap-all ${DRIVE}
+sgdisk --clear ${DRIVE}
 
+# Set optimal alignment for NVMe performance
+sgdisk --set-alignment=8 ${DRIVE}
+
+# Create partitions
+sgdisk --new=1:0:+2G \
+       --typecode=1:ef00 \
+       --change-name=1:"EFI" \
+       --new=2:0:0 \
+       --typecode=2:8300 \
+       --change-name=2:"ROOT" \
+       --attributes=2:set:2 \
+       ${DRIVE}
+       
+# Verify partitions
+sgdisk --verify ${DRIVE}
 partprobe ${DRIVE}
 
 # Format partitions
-mkfs.fat -F32 -n "EFI" ${DRIVE}p1
-mkfs.btrfs -f -L "ROOT" ${DRIVE}p2
+mkfs.fat -F32 -n EFI ${EFI_PART}
 
-# Initial mount for subvolume creation
-mount ${DRIVE}p2 /mnt
+# Format BTRFS with optimal settings for NVMe
+mkfs.btrfs -f \
+    -L ROOT \
+    -n 32k \
+    -m dup \
+    ${ROOT_PART}
 
-# Create subvolumes
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@root
-btrfs subvolume create /mnt/@var
-btrfs subvolume create /mnt/@cache
-btrfs subvolume create /mnt/@log
-btrfs subvolume create /mnt/@tmp
+# Mount ROOT partition
+mount ${ROOT_PART} /mnt
 
-# Unmount to prepare for final mount
+# Create BTRFS subvolumes
+cd /mnt
+btrfs subvolume create @
+btrfs subvolume create @home
+btrfs subvolume create @var
+btrfs subvolume create @log
+btrfs subvolume create @pkg
+btrfs subvolume create @.snapshots
+
+# Unmount and remount with subvolumes
+cd /
 umount /mnt
 
-# Create ALL mount points before mounting
-mkdir -p /mnt/{home,var/log,var/cache,root,srv,tmp,boot}
+# Mount subvolumes with optimal options
+mount -o noatime,compress=zstd:1,space_cache=v2,commit=120,subvol=@ ${ROOT_PART} /mnt
+mkdir -p /mnt/{home,var,var/log,var/cache/pacman/pkg,.snapshots,boot/efi}
+mount -o noatime,compress=zstd:1,space_cache=v2,commit=120,subvol=@home ${ROOT_PART} /mnt/home
+mount -o noatime,compress=zstd:1,space_cache=v2,commit=120,subvol=@var ${ROOT_PART} /mnt/var
+mount -o noatime,compress=zstd:1,space_cache=v2,commit=120,subvol=@log ${ROOT_PART} /mnt/var/log
+mount -o noatime,compress=zstd:1,space_cache=v2,commit=120,subvol=@pkg ${ROOT_PART} /mnt/var/cache/pacman/pkg
+mount -o noatime,compress=zstd:1,space_cache=v2,commit=120,subvol=@.snapshots ${ROOT_PART} /mnt/.snapshots
 
-# Now mount subvolumes in correct order
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@ ${DRIVE}p2 /mnt
-
-# Mount other subvolumes only after root is mounted
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@home ${DRIVE}p2 /mnt/home
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@root ${DRIVE}p2 /mnt/root
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@var ${DRIVE}p2 /mnt/var
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@cache ${DRIVE}p2 /mnt/var/cache
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@log ${DRIVE}p2 /mnt/var/log
-mount -o compress=zstd:1,noatime,space_cache=v2,subvol=@tmp ${DRIVE}p2 /mnt/tmp
-
-# Mount EFI partition last
-mount ${DRIVE}p1 /mnt/boot/efi
+# Mount EFI partition
+mount ${EFI_PART} /mnt/boot/efi
 
 # Verify mounts
-findmnt -t btrfs,vfat
+df -Th
+btrfs subvolume list /mnt
 
 # Install base system and AMD-specific packages
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware \
