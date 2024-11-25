44cf1d036cdd70dedbad1440df7a5a473c2daa7b
Harshal Sawant <harshalsawant2004h@gmail.com>
2024-11-25 08:23:25 +0530

    Update archinstaller.sh

--- archinstaller.sh
+++ archinstaller.sh
@@ -125,30 +125,6 @@ setup_filesystems() {
 }
 
 cachyos_repo_setup() {
-    # Add keys
-    # pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
-    # pacman-key --lsign-key F3B607488DB35A47
-
-    # Install CachyOS packages
-    # pacman -U --noconfirm \
-        # 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
-        # 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-18-1-any.pkg.tar.zst' \
-        # 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-18-1-any.pkg.tar.zst' \
-        # 'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r3.gf3211df-3.1-x86_64.pkg.tar.zst'
-
-    # Add CachyOS repositories to pacman.conf
-    # cat >> /etc/pacman.conf <<'CONF'
-# CachyOS repositories
-# [cachyos-v3]
-# Include = /etc/pacman.d/cachyos-v3-mirrorlist
-# [cachyos-core-v3]
-# Include = /etc/pacman.d/cachyos-v3-mirrorlist
-# [cachyos-extra-v3]
-# Include = /etc/pacman.d/cachyos-v3-mirrorlist
-# [cachyos]
-# Include = /etc/pacman.d/cachyos-mirrorlist
-# CONF
-
     curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
     tar xvf cachyos-repo.tar.xz
     cd cachyos-repo
@@ -168,10 +144,28 @@ EOF
 install_base_system() {
     info "Installing base system..."
 
-    curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
-    tar xvf cachyos-repo.tar.xz
-    cd cachyos-repo
-    ./cachyos-repo.sh
+    # Add keys
+     pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+     pacman-key --lsign-key F3B607488DB35A47
+
+    # Install CachyOS packages
+      pacman -U --noconfirm \
+         'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+         'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-18-1-any.pkg.tar.zst' \
+         'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-18-1-any.pkg.tar.zst' \
+         'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r3.gf3211df-3.1-x86_64.pkg.tar.zst'
+
+    # Add CachyOS repositories to pacman.conf
+     cat >> /etc/pacman.conf <<'CONF'
+[cachyos-v3]
+Include = /etc/pacman.d/cachyos-v3-mirrorlist
+[cachyos-core-v3]
+Include = /etc/pacman.d/cachyos-v3-mirrorlist
+[cachyos-extra-v3]
+Include = /etc/pacman.d/cachyos-v3-mirrorlist
+[cachyos]
+Include = /etc/pacman.d/cachyos-mirrorlist
+CONF
 
     mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
     cat > /etc/pacman.d/mirrorlist <<'EOF'
@@ -181,7 +175,7 @@ EOF
 
     # Update package database
     pacman -Syy --noconfirm
-
+    
     local base_packages=(
         # Core System
         base base-devel
@@ -236,7 +230,7 @@ Server = https://mirror.sahil.world/archlinux/$repo/os/$arch
 EOF
 
     # Update package database
-    pacman -Syy --noconfirm
+    pacman -Syyu
     
     # Set timezone and clock
     ln -sf /usr/share/zoneinfo/${CONFIG[TIMEZONE]} /etc/localtime
