bc65961e59e737f8a002c4adf5cae89dbe1b75e1
Harshal Sawant <harshalsawant2004h@gmail.com>
2024-12-10 20:40:07 +0530

    Update archclone.sh

--- archclone.sh
+++ archclone.sh
@@ -1,4 +1,3 @@
-git clone https://gist.github.com/7e0f93c67fd2ce458d1b941e4a3a2424.git
-cd 7*
+git clone https://gist.github.com/7e0f93c67fd2ce458d1b941e4a3a2424.git && cd 7*
 chmod +x ./archinstall.sh
 sh ./archinstall.sh
